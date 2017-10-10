6d355540a4629f343db8ecfa6dc71466c69e9643
Victor Guillem Agustí <vidasoft.app@gmail.com>
2017-10-10 23:52:53 +0200

    Añadido traductorbot

--- kik/index.js
+++ kik/index.js
@@ -2,19 +2,49 @@
 
 let util = require('util');
 let http = require('http');
+let TraductorBot = require('traductor_bot');
 let Bot  = require('@kikinteractive/kik');
 
 // Configure the bot API endpoint, details for your bot
 let bot = new Bot({
-    username: 'echo.bot',
-    apiKey: 'b225d75a-355b-4e6e-b8de-60cd869bfbbf',
-    baseUrl: 'https://kik-echobot.ngrok.io/'
+    username: 'ejemplotraductor',
+    apiKey: '4ef58753-d764-4ab9-a34d-5f19e8d0b361',
+    baseUrl: 'https://a0b6dfff.ngrok.io/'
 });
 
 bot.updateBotConfiguration();
 
+function enviarMensaje() {
+	let jsonGenerico = { 
+        "idReceptor": "001", 
+        "idCanal": "001", 
+        "contenido": { 
+          "texto": "hola mundo", 
+          "tipo": "text"
+        }
+    };
+	return TraductorBot.KikBot.translateToBot(jsonGenerico);
+}
+enviarMensaje();
+
 bot.onTextMessage((message) => {
-    message.reply(message.body);
+	// console.log(Bot.Message.text("Holi"));
+    // message.reply(enviarMensaje());
+	console.log(message.body);
+	let election = message.body.split(" ");
+	console.log(election[0]);
+	switch(election[0]){
+		case "/image":
+			message.reply(Bot.Message.picture('http://i.imgur.com/oalyVlU.jpg'));
+		break;
+		case "/text":
+
+			message.reply(election[1]);
+		break;
+		default:
+			message.reply("Holi, no se ha recibio na de na men.");
+		break;
+	}
 });
 
 // Set up your server and start listening
